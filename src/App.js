22a2083edb15e58443f878f9a840deb98109241c
RachhanaBaliga-Dev <rachhana.baliga@gmail.com>
2025-11-29 19:22:00 -0500

    added some interactivity to this app

--- src/App.js
+++ src/App.js
@@ -1,30 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
 import TestComponent from "./components/TestComponent";
+import {useState} from "react";
 
 const Comp = () => {
-  return <p>This is a comp component</p>
+  return <p>How many turkeys did you have this thanksgiving??</p>
 }
 
 function App() {
+    const [count, setCount] = useState(1);
   return (
     <div className="App" id='root'>
             <TestComponent />
 
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         <u>SI 579 Fall 2025</u>
         </p>
+          <p>
+              Created by: <strong>Rachhana Saish Baliga ( uniqname: rachhana )</strong>
+          </p>
         <Comp />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <button
+              onClick={() => setCount(count + 1)}
+          > More!!! </button>
+          <div>
+              {'🦃'.repeat(count)}
+          </div>
       </header>
     </div>
   );
